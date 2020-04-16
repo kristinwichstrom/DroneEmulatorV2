ff5b1c046b815ce21c0f553c7fe0cb7f73a9eb7d
kristinwichstrom <kristinwichstrom@gmail.com>
2020-04-16 10:02:50 +0200

    First commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void toggleBtnDrone(ActionEvent actionEvent) {
+        System.out.println("toggle Drone button");
+    }
+
+    public void toggleBtnBroadcast(ActionEvent actionEvent) {
+        System.out.println("Toggle Broadcast button");
+    }
+}
