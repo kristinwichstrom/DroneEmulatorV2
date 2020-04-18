42762e6f0bd943a2ff063ac77e2e00272ced9a66
kristinwichstrom <kristinwichstrom@gmail.com>
2020-04-18 14:53:36 +0200

    Controller class updated

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,24 +1,38 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
 
 public class Controller {
 
-    BroadcastServer broadcastServer;
+    @FXML
+    TableView<Messages> table;
+    @FXML
+    ToggleButton toggleBtnDrone;
+    @FXML
+    ToggleButton toggleBtnBroadcast;
 
-    //Method creates an instance of the BroadcastServer class and starts a thread for the class
-    public void initBroadcastServer() {
+    private UdpConnector udpConnector;
+    private BroadcastServer broadcastServer;
 
-        broadcastServer = new BroadcastServer();
-        new Thread(broadcastServer).start();
-    }
+    //Method creates an instance of the BroadcastServer class and starts a thread for the clas
 
     public void toggleBtnDrone(ActionEvent actionEvent) {
         System.out.println("toggle Drone button");
+        if (udpConnector.isReceiveMessages()){
+
+            udpConnector.setReceiveMessages(false);
+            toggleBtnDrone.setText("OFF");
+        } else
+            startUdpConnection();
+        toggleBtnDrone.setText("ON");
     }
 
     public void toggleBtnBroadcast(ActionEvent actionEvent) {
         System.out.println("Toggle Broadcast button");
+        if
     }
 
     public void clearTable() {
@@ -30,5 +44,27 @@ public class Controller {
     }
     public void handleMessages () {
         //Updating table
+    }
+    public void initialize()
+    {
+        System.out.println("Initialize broadcasting");
+
+        startUdpConnection();
+        startBroadcasting();
+    }
+    private void startBroadcasting() {
+        broadcastServer = new BroadcastServer();
+        new Thread(broadcastServer).start();
+    }
+
+    private void startUdpConnection() {
+        if (udpConnector != null) udpConnector.closeSocket();
+        udpConnector = new UdpConnector(this);
+        new Thread(udpConnector).start();
+    }
+
+    public void receiveMessage(Messages udpMessage)
+    {
+        table.getItems().add(0, udpMessage);
     }
 }
