adf2a435ebda7c0cb51da3a078a3a9ce5e54b63f
kristinwichstrom <kristinwichstrom@gmail.com>
2020-04-19 01:35:06 +0200

    Issues with Controller - selectSquare function

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -76,7 +76,7 @@ public class Controller {
         //Change color of circle
         // Move circle around
     }
-
+//needs fix
     public void selectSquare(ActionEvent actionEvent) {
         //selectedTool = new DrawDrone();
         System.out.println("Circle square");
